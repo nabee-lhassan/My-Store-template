e9f52a8d46bd2b6cec9552f44801bf147959efc6
nabee-lhassan <your.nabeelhassa02@gmail.com>
2024-07-01 17:48:50 +0500

    1/7/2024

--- src/Component/CatDetail.jsx
+++ src/Component/CatDetail.jsx
@@ -6,10 +6,14 @@ import { addWishlist } from "../redux/Slice/wishlistSlice";
 import { setPop } from "../redux/Slice/PopSlice";
 import Heading from "./Heading";
 import Rating from "./Rating";
-import { useState, useMemo } from "react";
+import { useMemo, useState } from "react";
+import { IoGridOutline } from "react-icons/io5";
+import { CiBoxList } from "react-icons/ci";
 
 export default function CatDetail() {
+  // Destructure the specific parameter from the URL
   const { name } = useParams();
+
   const dispatch = useDispatch();
 
   function handle(product) {
@@ -19,19 +23,25 @@ export default function CatDetail() {
 
   const wishlistsHandle = (product) => {
     dispatch(addWishlist(product));
-  };
+  }
 
   const products = useSelector((state) => state.products.data);
 
+  // Filter products based on the category
+  // const filteredProducts = products.filter((element) => element.category === name);
+
+  
   const filteredProducts = useMemo(() => 
     products.filter((element) => element.category === name), 
     [products, name]
   );
 
+  // Log prices of filtered products
   const filteredPrices = filteredProducts.map((product) => product.price);
 
-  const maxPrice = filteredPrices.length > 0 ? Math.max(...filteredPrices) : 0;
-  const minPrice = filteredPrices.length > 0 ? Math.min(...filteredPrices) : 0;
+  // Find the maximum and minimum price, handle empty filteredPrices case
+  const maxPrice = filteredPrices.length > 0 ? Math.max(...filteredPrices) : '';
+  const minPrice = filteredPrices.length > 0 ? Math.min(...filteredPrices) : '';
 
   const [price, setPrice] = useState(maxPrice);
 
@@ -39,18 +49,26 @@ export default function CatDetail() {
     setPrice(Number(event.target.value));
   };
 
+
+  const priceFilteredProducts = filteredProducts.filter((product)=> 
+    product.price <= price 
+  )
+  
+
+  // const priceFilteredProducts = filteredProducts.filter(
+  //   (product) => product.price <= price
+  // );
+
   const totalProducts = filteredProducts.length;
 
   const state = useSelector((state) => state);
 
+
+  
+
   if (state.products.isLoading) {
     return <h1>Loading...</h1>;
   }
-
-  // Filter products based on the current price
-  const priceFilteredProducts = filteredProducts.filter(
-    (product) => product.price <= price
-  );
 
   return (
     <div>
@@ -65,9 +83,11 @@ export default function CatDetail() {
             Search By Category
           </button>
           <ul className="dropdown-menu">
-            {Array.from(new Set(products.map((uniqueCate) => uniqueCate.category))).map((itemCat) => (
-              <li key={itemCat}><Link className="dropdown-item" to={`/category/${itemCat}`}>{itemCat}</Link></li>
-            ))}
+            {
+              Array.from(new Set(products.map((uniqueCate) => uniqueCate.category))).map((itemCat) => (
+                <li key={itemCat}><Link className="dropdown-item" to={`/category/${itemCat}`}>{itemCat}</Link></li>
+              ))
+            }
           </ul>
         </div>
         <div className="div-priceFilter">
@@ -78,9 +98,17 @@ export default function CatDetail() {
         </div>
       </aside>
       <aside style={{ width: '80%', float: 'right' }} className="side-right">
+        <div className="filter-container container">
+          <div className="row">
+            <div className="col-lg-12">
+              <button className="btn"><IoGridOutline /></button>
+              <button className="btn"><CiBoxList /></button>
+            </div>
+          </div>
+        </div>
         <div className="container all-Product">
           <div className="row">
-            {priceFilteredProducts.length > 0 ? priceFilteredProducts.map((item) => (
+            {priceFilteredProducts.length > 0 ? priceFilteredProducts .map((item) => (
               <div className="col-lg-3 col-md-4 col-sm-1 div-card" key={item.id}>
                 <div className="div-image">
                   <button className="wishlist btn" onClick={() => wishlistsHandle(item)}>
@@ -90,7 +118,7 @@ export default function CatDetail() {
                   </button>
                   <button className="cart_btn btn" onClick={() => handle(item)}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" className="bi bi-cart3" viewBox="0 0 16 16">
-                      <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+                      <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
                     </svg>
                   </button>
                   <Link to={`/detail/${item.id}`}>
